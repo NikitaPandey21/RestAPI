02424dead4bc8b9f21f71718ea6ba966ebd9ce7d
NikitaPandey21 <nikitarapandey@gmail.com>
2022-09-17 20:35:05 +0530

    added put and delete methods and introduced parser

--- Authentication/app.py
+++ Authentication/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource,Api 
+from flask_restful import Resource,Api, reqparse
 from flask_jwt import JWT, jwt_required 
 
 from security import authenticate, identity 
@@ -13,6 +13,13 @@ jwt = JWT(app, authenticate, identity) # created /auth
 items = [] 
 
 class Items(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be blank!"
+    ) # parser will accept only arguments mentioned in add_arguments others will be ignored
+
     @jwt_required()
     def get(self,name):  
         item = next(filter(lambda x: x['name']==name,items), 'Item Not found') # next gives first item matched 
@@ -24,10 +31,29 @@ class Items(Resource):
     def post(self,name):
         if next(filter(lambda x: x['name']==name,items), None) is not None:
             return {"message": "Item with name {} already exist".format(name)}, 400
-        data = request.get_json()
+
+        # data = request.get_json()
+        data = Items.parser.parse_args()
+        
         item = {"name": name,"price":data['price']}
         items.append(item)
         return item, 201 # code 201 is for created , 202 for accepted you can use when creating is taking long time
+
+    def delete(self,name):
+        global items 
+        items = list(filter(lambda x: x['name']!=name,items))
+        return {"message": "item {} deleted".format(name)}
+
+    def put(self,name):
+        data = Items.parser.parse_args()
+
+        item = next(filter(lambda x: x['name'] == name, items),None)
+        if item is None:
+            item = {"name": name,"price": data['price']}
+            items.append(item)
+        else: 
+            item.update(data)
+        return item
 
 class ItemsList(Resource):
     def get(self):
